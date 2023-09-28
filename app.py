565612272ed1ffe0b6cee030beceba267ade4878
Isaac Ige <heywhydot16@gmail.com>
2023-09-28 13:45:54 +0100

    got the post and form  method working

--- app.py
+++ app.py
@@ -15,27 +15,28 @@ leap = Leap(
 async def index():
     return render_template('index.html')
 
-@app.route('/generateimage')
+@app.route('/generateimage', methods=['GET', 'POST'])
 def generate():
     try:
         if request.method == 'GET':
-            return render_template('index.html')
-        else:
+            return render_template('generateImage.html')
+        elif request.method == 'POST':
             # Generate an Image
             generate_response = leap.images.generate(
-
                 prompt= request.form.get('floating_prompt'),  # required
                 model_id="26a1a203-3a46-42cb-8cfa-f4de075907d8",  # required
                 negative_prompt=request.form.get("floating_negative_prompt"),  # optional
-                steps=50,  # optional
-                width=1024,  # optional
-                height=1024,  # optional
-                number_of_images=1,  # optional
+                steps=int(request.form.get('floating_steps')),  # optional
+                width=int(request.form.get('floating_width')),  # optional
+                height=int(request.form.get('floating_height')),  # optional
+                number_of_images=int(request.form.get('floating_number_of_images')),  # optional
                 prompt_strength=7,  # optional
                 seed=4523184,  # optional
             )
             pprint(generate_response.body)
-            return render_template('generateImage.html', image=generate_response.body)
+            return render_template('generateImage.html', image=generate_response.status)
+        else:
+            print(request.method)
     except ApiException as e:
         print("Exception when calling ImagesApi.generate: %s\n" % e)
         pprint(e.body)
